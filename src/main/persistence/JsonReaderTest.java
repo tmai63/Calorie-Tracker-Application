f3306b9bfa41ff60134073c3f7f51b34623f04fa
Thomson <thomsonmai63@gmail.com>
2021-10-28 12:23:08 -0700

    - added tests for JsonReader

--- src/main/persistence/JsonReaderTest.java
+++ src/main/persistence/JsonReaderTest.java
@@ -1,11 +1,54 @@
 package persistence;
 
+import model.Day;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 
 public class JsonReaderTest {
 
+    @Test
+    void testReaderNonExistentFile() {
+        JsonReader reader = new JsonReader("./data/noSuchFile.json");
+        try {
+            Day day = reader.read();
+            fail("IOException expected");
+        } catch (IOException e) {
+            // pass
+        }
+    }
+
+    @Test
+    void testReaderEmptyWorkRoom() {
+        JsonReader reader = new JsonReader("./data/testReaderEmptyWorkRoom.json");
+        try {
+            Day day = reader.read();
+            assertEquals(LocalDate.now(), day.returnDate());
+            assertEquals(0, day.numItems());
+        } catch (IOException e) {
+            fail("Couldn't read from file");
+        }
+    }
+
+    @Test
+    void testReaderGeneralWorkRoom() {
+        JsonReader reader = new JsonReader("./data/testReaderGeneralWorkRoom.json");
+        try {
+            Day day = reader.read();
+            assertEquals(LocalDate.now(), day.returnDate());
+            assertEquals(5, day.numItems());
+            assertEquals(1500, day.returnCalorieTarget());
+        } catch (IOException e) {
+            fail("Couldn't read from file");
+        }
+    }
 
 
 }
