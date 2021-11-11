9af6f0ea34ccb4453c4ca4c95cdf4c9b9c23ff48
Thomson <thomsonmai63@gmail.com>
2021-11-10 21:06:22 -0800

    - tests for meal order

--- src/test/model/FoodTest.java
+++ src/test/model/FoodTest.java
@@ -10,10 +10,16 @@ import org.json.JSONObject;
 class FoodTest {
 
     Food hamburger;
+    Food cereal;
+    Food pizza;
+    Food yogurt;
 
     @BeforeEach
     void runBefore() {
+        cereal = new Food("Breakfast", "cereal", 200);
         hamburger = new Food("Lunch", "Hamburger", 600);
+        pizza = new Food("Dinner", "Pizza", 700);
+        yogurt = new Food("Snack", "Yogurt", 150);
     }
 
     @Test
@@ -31,10 +37,11 @@ class FoodTest {
         assertEquals("Hamburger", hamburger.getFoodName());
     }
 
-//    @Test
-//    void testToJson() {
-//        JSONObject jsonObject = new JSONObject();
-//        String json = "{\"Meal\":\"Lunch\",\"Calories\":600,\"Name\":\"Hamburger\"}";
-//        assertEquals(json,hamburger.toJson());
-//    }
+    @Test
+    void testFoodOrder() {
+        assertEquals(2,hamburger.getMealOrder());
+        assertEquals(1,cereal.getMealOrder());
+        assertEquals(3,pizza.getMealOrder());
+        assertEquals(4,yogurt.getMealOrder());
+    }
 }
